53b160f8395ca5ce7c44ebc77ea604709cbe8272
Katie Veneziano <>
2021-01-15 12:57:37 -0500

    Updated resume to include accessibility tools

--- src/ResumeRender.js
+++ src/ResumeRender.js
@@ -4,7 +4,7 @@ function Resume (props) {
   return (
     <div>
       <h1>Resume</h1>
-      <iframe src="https://drive.google.com/file/d/1Fc-XM3C6CQ-9zgnPzD3oRC0hzd3DYNi2/preview" width="640" height="480"></iframe>
+      <iframe src="https://drive.google.com/file/d/1y-MrD-xc6NCZAcHKEPm8cNKGwIn3OMVr/preview" width="640" height="480"></iframe>
     </div>
   )
 }
