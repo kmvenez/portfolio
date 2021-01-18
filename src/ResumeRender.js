0597decfa3184d1d80c1c0bb847b5dda2ee036d9
Katie Veneziano <>
2021-01-18 11:28:41 -0500

    Updated to include UOCT Certificate Screenshot

--- src/ResumeRender.js
+++ src/ResumeRender.js
@@ -6,7 +6,9 @@ function Resume (props) {
       <h1>Resume</h1>
       <iframe src="https://drive.google.com/file/d/1y-MrD-xc6NCZAcHKEPm8cNKGwIn3OMVr/preview" width="640" height="480"></iframe><br/>
       <h1>General Assembly</h1>
-      <iframe src="https://drive.google.com/file/d/1Pi7vh36eUvXM8kTXwOZoCaax2hnDBYNY/preview" width="640" height="480"></iframe>
+      <iframe src="https://drive.google.com/file/d/1Pi7vh36eUvXM8kTXwOZoCaax2hnDBYNY/preview" width="640" height="480"></iframe><br/>
+      <h1>University of Cape Town</h1>
+      <iframe src="https://drive.google.com/file/d/1BON2md54Wl83xC8MPmTcHlGPHPhhsVV3/preview" width="640" height="480"></iframe>
     </div>
   )
 }
